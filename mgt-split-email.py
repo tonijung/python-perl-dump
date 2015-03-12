e8ccc62a592d45d0d5bd59990e950bb4ae8e51fa
Toni Jung <tjung@mail.usf.edu>
2015-03-12 10:43:09 -0700

    Create mgt-split-email.py

---
+++ mgt-split-email.py
@@ -0,0 +1,32 @@
+from PyPDF2 import PdfFileWriter, PdfFileReader
+import os, sys
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+from email.MIMEImage import MIMEImage
+
+# [1] split pdf from RNW
+inputpdf = PdfFileReader(open("grades-main.pdf", "rb"))
+
+for i in xrange(inputpdf.numPages):
+    output = PdfFileWriter()
+    output.addPage(inputpdf.getPage(i))
+    with open("grade-%s.pdf" % i, "wb") as outputStream:
+        output.write(outputStream)
+        
+# [2] rename files
+students = ["ROSTER HERE"] #import from CSV
+numstudent = range(1,37)
+for student, i in zip(students, numstudent):
+        os.rename("grade-%s.pdf" % i, "%s.pdf" % student)
+
+# [3] send out emails
+stuemails = ["LIST HERE"]
+msg = MIMEMultipart()
+#msg.attach(MIMEText(file("/home/myuser/sample.pdf").read())
+msg.attach(MIMEText(file("C:/test/Choice.pdf").read())
+
+mailer = smtplib.SMTP()
+mailer.connect()
+mailer.sendmail(from_, to, msg.as_string())
+mailer.close()
